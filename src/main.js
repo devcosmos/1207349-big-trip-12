0cd5943c8135c12dbef3aca5d23257b21d3b2eed
Litvinov Valery <va-litv@yandex.ru>
2020-08-02 17:33:27 +0300

    M2T5 moved the function createTripInfoTemplate into separate file

--- src/main.js
+++ src/main.js
@@ -1,17 +1,6 @@
-"use strict";
+import {createTripInfoTemplate} from "./view/trip-info.js";
 
 const EVENT_COUNT = 3;
-
-const createTripInfoTemplate = () => {
-  return (
-    ` <section class="trip-main__trip-info  trip-info">
-      <div class="trip-info__main">
-        <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
-        <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
-      </div>
-    </section>`
-  );
-};
 
 const createTotalPriceTemplate = () => {
   return (
