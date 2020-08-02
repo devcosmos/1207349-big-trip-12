82a301e5c35d4c147ff2dfdd0fdbe6b2cbfe41ed
Litvinov Valery <va-litv@yandex.ru>
2020-08-02 17:41:23 +0300

    M2T5 moved the function createEventFiltrationTemplate into separate file

--- src/main.js
+++ src/main.js
@@ -1,31 +1,9 @@
 import {createTripInfoTemplate} from "./view/trip-info.js";
 import {createTotalPriceTemplate} from "./view/total-price.js";
 import {createNavigationControllerTemplate} from "./view/nav-controller.js";
+import {createEventFiltrationTemplate} from "./view/event-filtration.js";
 
 const EVENT_COUNT = 3;
-
-const createEventFiltrationTemplate = () => {
-  return (
-    `<form class="trip-filters" action="#" method="get">
-      <div class="trip-filters__filter">
-        <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
-        <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-      </div>
-
-      <div class="trip-filters__filter">
-        <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-        <label class="trip-filters__filter-label" for="filter-future">Future</label>
-      </div>
-
-      <div class="trip-filters__filter">
-        <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
-        <label class="trip-filters__filter-label" for="filter-past">Past</label>
-      </div>
-
-      <button class="visually-hidden" type="submit">Accept filter</button>
-    </form>`
-  );
-};
 
 const createSortingTemplate = () => {
   return (
