a515713557f1a0162075622b332306128cc5bc2c
Litvinov Valery <va-litv@yandex.ru>
2020-08-07 10:12:05 +0300

    M3T7 created function for generating random description

--- src/mock/event.js
+++ src/mock/event.js
@@ -1,10 +1,27 @@
+import {getRandomInteger} from "../utils";
+
+const generateDescription = () => {
+  const descriptions = [
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+    `Cras aliquet varius magna, non porta ligula feugiat eget. `,
+    `Fusce tristique felis at fermentum pharetra.`,
+    `Aliquam id orci ut lectus varius viverra.`,
+    `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
+    `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`
+  ];
+
+  const randomIndex = getRandomInteger(0, descriptions.length - 1);
+
+  return descriptions[randomIndex];
+}
+
 export const generateEvent = () => {
   return {
     typeEvent: `Taxi`,
     destination: `Amsterdam`,
     offers: ``,
     description: {
-      text: `lorem`,
+      text: generateDescription(),
       photo: `http://picsum.photos/248/152?r=${Math.random()}`
     },
     startDate: null,
