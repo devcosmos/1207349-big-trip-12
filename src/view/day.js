ca8bfe75535ca6f96c52f70d00498c364329f3b7
Litvinov Valery <va-litv@yandex.ru>
2020-08-10 17:16:14 +0300

    M3T7 renamed function

--- src/view/day.js
+++ src/view/day.js
@@ -1,19 +1,23 @@
-const getEventTimeFrontend = (timestemp) => {
+const getDaysDateFrontend = (timestemp) => {
   return timestemp.toLocaleString(`en-US`, {month: `short`, day: `2-digit`});
 };
 
-const getEventTimeBackend = (timestemp) => {
+const getDaysDateBackend = (timestemp) => {
   const date = new Date();
   date.setTime(timestemp);
+
   return date.getFullYear() + `-` + (`0` + (date.getMonth() + 1)).slice(-2) + `-` + (`0` + (date.getDate() + 1)).slice(-2);
 };
 
 export const createDayTemplate = (date, index) => {
+  const daysDateFrontend = getDaysDateFrontend(new Date(date));
+  const daysDateBackend = getDaysDateBackend(date);
+
   return (
     `<li class="trip-days__item  day">
       <div class="day__info">
         <span class="day__counter">${index}</span>
-        <time class="day__date" datetime="${getEventTimeBackend(date)}">${getEventTimeFrontend(new Date(date))}</time>
+        <time class="day__date" datetime="${daysDateBackend}">${daysDateFrontend}</time>
       </div>
 
       <ul class="trip-events__list" id="trip-events__list-${index}"></ul>
