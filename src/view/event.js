88b283afe8ba913aed79635a7ac5120519070e90
Litvinov Valery <va-litv@yandex.ru>
2020-08-10 11:33:17 +0300

    M3T7 outputted the function up

--- src/view/event.js
+++ src/view/event.js
@@ -1,63 +1,63 @@
 import {isTransport} from "../utils";
 
+const addZero = (i) => {
+  if (i < 10) {
+    i = `0` + i;
+  }
+
+  return i;
+};
+
+const getEventTimeFrontend = (date) => {
+  const h = addZero(date.getHours());
+  const m = addZero(date.getMinutes());
+
+  return `${h}:${m}`;
+};
+
+const getEventTimeBackend = (date) => {
+  const y = date.getFullYear();
+  const m = addZero(date.getMonth());
+  const d = addZero(date.getDate());
+
+  return `${y}:${m}:${d}T${getEventTimeFrontend(date)}`;
+};
+
+const getDifference = (start, end) => {
+  const difference = (Date.parse(end) - Date.parse(start));
+
+  let minuts = difference / (1000 * 60);
+  if (minuts >= 60) {
+    let hours = Math.floor(minuts / 60);
+    minuts = Math.floor(minuts % (hours * 60));
+
+    if (minuts === 0) {
+      return `${hours}H`;
+    }
+
+    return `${hours}H ${minuts}M`;
+  }
+
+  return `${minuts}M`;
+};
+
+const createAcceptedOffersTemplate = (offers) => {
+  return (
+    offers !== 0
+      ? `<h4 class="visually-hidden">Offers:</h4>
+        <ul class="event__selected-offers">
+          ${Array.from(offers).map((offer) => `<li class="event__offer">
+            <span class="event__offer-title">${offer.name}</span>
+            &plus;
+            &euro;&nbsp;<span class="event__offer-price">${offer.cost}</span>
+          </li>`).join(``)}
+        </ul>`
+      : ``
+  );
+};
+
 export const createEventTemplate = (event) => {
   const {eventType, currentDestination, acceptedOffers, dateStart, dateEnd, cost} = event;
-
-  const addZero = (i) => {
-    if (i < 10) {
-      i = `0` + i;
-    }
-
-    return i;
-  };
-
-  const getEventTimeFrontend = (date) => {
-    const h = addZero(date.getHours());
-    const m = addZero(date.getMinutes());
-
-    return `${h}:${m}`;
-  };
-
-  const getEventTimeBackend = (date) => {
-    const y = date.getFullYear();
-    const m = addZero(date.getMonth());
-    const d = addZero(date.getDate());
-
-    return `${y}:${m}:${d}T${getEventTimeFrontend(date)}`;
-  };
-
-  const getDifference = (start, end) => {
-    const difference = (Date.parse(end) - Date.parse(start));
-
-    let minuts = difference / (1000 * 60);
-    if (minuts >= 60) {
-      let hours = Math.floor(minuts / 60);
-      minuts = Math.floor(minuts % (hours * 60));
-
-      if (minuts === 0) {
-        return `${hours}H`;
-      }
-
-      return `${hours}H ${minuts}M`;
-    }
-
-    return `${minuts}M`;
-  };
-
-  const createAcceptedOffersTemplate = (offers) => {
-    return (
-      offers !== 0
-        ? `<h4 class="visually-hidden">Offers:</h4>
-          <ul class="event__selected-offers">
-            ${Array.from(offers).map((offer) => `<li class="event__offer">
-              <span class="event__offer-title">${offer.name}</span>
-              &plus;
-              &euro;&nbsp;<span class="event__offer-price">${offer.cost}</span>
-            </li>`).join(``)}
-          </ul>`
-        : ``
-    );
-  };
 
   const prepositions = isTransport(eventType) ? `in` : `to`;
   const eventStartTimeFrontend = getEventTimeFrontend(dateStart);
