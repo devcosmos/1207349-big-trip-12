99e7603fbb4db984de682ee3199ada1c33d0c0ca
Litvinov Valery <va-litv@yandex.ru>
2020-08-26 12:08:13 +0300

    M4T11 refactored main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import {TripInfoView, NavigationControllerView, EventFiltrationView, TotalPriceV
 import {generateEvent, DESTINATIONS} from "./mock/event";
 import {splitEventsByDays, renderElement} from "./utils";
 
+const events = new Array(EVENT_COUNT).fill().map(generateEvent).sort((a, b) => a.dateStart - b.dateStart);
+
+const siteHeaderElement = document.querySelector(`.page-header`);
+const siteMainElement = document.querySelector(`.page-main`);
+const tripElement = siteHeaderElement.querySelector(`.trip-main`);
+const eventsElement = siteMainElement.querySelector(`.trip-events`);
+const tripControlsFirstElement = tripElement.querySelector(`.trip-controls > h2:first-child`);
+const tripControlsSecondElement = tripElement.querySelector(`.trip-controls > h2:last-child`);
+
 const renderEvent = (event) => {
   const eventComponent = new EventView(event).getElement();
   const eventEditComponent = new EventEditorView(event, DESTINATIONS).getElement();
@@ -39,38 +48,36 @@ const renderEvent = (event) => {
   return eventComponent;
 };
 
-const events = new Array(EVENT_COUNT).fill().map(generateEvent).sort((a, b) => a.dateStart - b.dateStart);
-const eventsByDays = splitEventsByDays(events);
-const daysComponent = new DaysView();
-const tripInfoComponent = new TripInfoView(events);
+const renderBoard = (container, boardEvents) => {
+  const tripInfoComponent = new TripInfoView(boardEvents);
 
-const siteHeaderElement = document.querySelector(`.page-header`);
-const siteMainElement = document.querySelector(`.page-main`);
-const tripElement = siteHeaderElement.querySelector(`.trip-main`);
-const eventsElement = siteMainElement.querySelector(`.trip-events`);
-const tripControlsFirstElement = tripElement.querySelector(`.trip-controls > h2:first-child`);
-const tripControlsSecondElement = tripElement.querySelector(`.trip-controls > h2:last-child`);
+  renderElement(tripElement, tripInfoComponent.getElement(), RENDER_POSITION.AFTERBEGIN);
+  renderElement(tripInfoComponent.getElement(), new TotalPriceView().getElement(), RENDER_POSITION.BEFOREEND);
 
-renderElement(tripElement, tripInfoComponent.getElement(), RENDER_POSITION.AFTERBEGIN);
+  if (boardEvents.length === 0) {
+    renderElement(container, new NoEventView().getElement(), RENDER_POSITION.BEFOREEND);
+    return;
+  }
+
+  const daysComponent = new DaysView();
+  const eventsByDays = splitEventsByDays(boardEvents);
+
+  renderElement(container, new SortingView().getElement(), RENDER_POSITION.BEFOREEND);
+  renderElement(container, daysComponent.getElement(), RENDER_POSITION.BEFOREEND);
+
+  eventsByDays.forEach((eventsByDay, index) => {
+    const dayDate = eventsByDay[0];
+    const dayComponent = new DayView(dayDate, index + 1);
+    const eventsListElement = dayComponent.getElement().querySelector(`#trip-events__list-${index + 1}`);
+
+    eventsByDay.slice(1).forEach((event) => {
+      eventsListElement.append(renderEvent(event));
+    });
+
+    daysComponent.getElement().append(dayComponent.getElement());
+  });
+};
+
 renderElement(tripControlsFirstElement, new NavigationControllerView().getElement(), RENDER_POSITION.AFTEREND);
 renderElement(tripControlsSecondElement, new EventFiltrationView().getElement(), RENDER_POSITION.AFTEREND);
-renderElement(tripInfoComponent.getElement(), new TotalPriceView().getElement(), RENDER_POSITION.BEFOREEND);
-
-if (events.length === 0) {
-  renderElement(eventsElement, new NoEventView().getElement(), RENDER_POSITION.BEFOREEND);
-} else {
-  renderElement(eventsElement, new SortingView().getElement(), RENDER_POSITION.BEFOREEND);
-  renderElement(eventsElement, daysComponent.getElement(), RENDER_POSITION.BEFOREEND);
-}
-
-eventsByDays.forEach((eventsByDay, index) => {
-  const dayDate = eventsByDay[0];
-  const dayComponent = new DayView(dayDate, index + 1);
-  const eventsListElement = dayComponent.getElement().querySelector(`#trip-events__list-${index + 1}`);
-
-  eventsByDay.slice(1).forEach((event) => {
-    eventsListElement.append(renderEvent(event));
-  });
-
-  daysComponent.getElement().append(dayComponent.getElement());
-});
+renderBoard(eventsElement, events);
