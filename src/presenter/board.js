d6feda40ee2be75995f22da4dd438daa8b656263
Litvinov Valery <va-litv@yandex.ru>
2020-08-28 15:32:12 +0300

    M5T14 added renderEvent in Trip presentor

--- src/presenter/board.js
+++ src/presenter/board.js
@@ -1,7 +1,8 @@
-import {TripInfoView, TotalPriceView, SortingView, DaysView, DayView, NoEventView} from "../view/index";
+import {TripInfoView, TotalPriceView, SortingView, EventEditorView, DaysView, DayView, EventView, NoEventView} from "../view/index";
 import {splitEventsByDays} from "../utils/event";
-import {renderElement} from "../utils/render";
+import {renderElement, replaceElement} from "../utils/render";
 import {RENDER_POSITION} from "../const";
+import {DESTINATIONS} from "../mock/event";
 
 export default class Board {
   constructor(boardContainer, tripMainContainer) {
@@ -11,6 +12,8 @@ export default class Board {
     this._totalPriceComponent = new TotalPriceView();
     this._sortingComponent = new SortingView();
     this._daysComponent = new DaysView();
+    this._eventComponent = new EventView();
+    this._eventEditComponent = new EventEditorView();
     this._noEventComponent = new NoEventView();
   }
 
@@ -24,20 +27,60 @@ export default class Board {
     this._renderBoard();
   }
 
-  _renderSorting() {
-    renderElement(this._boardContainer, this._sortingComponent, RENDER_POSITION.BEFOREEND);
-  }
+  _renderEvent(event) {
+    const eventComponent = new EventView(event);
+    const eventEditComponent = new EventEditorView(event, DESTINATIONS);
 
-  _renderDays() {
-    renderElement(this._boardContainer, this._daysComponent, RENDER_POSITION.BEFOREEND);
+    const replacePointToForm = () => {
+      replaceElement(eventEditComponent, eventComponent);
+    };
+
+    const replaceFormToPoint = () => {
+      replaceElement(eventComponent, eventEditComponent);
+    };
+
+    const onEscKeyDown = (evt) => {
+      if (evt.key === `Escape`) {
+        replaceFormToPoint();
+        document.removeEventListener(`keydown`, onEscKeyDown);
+      }
+    };
+
+    eventComponent.setEditClickHandler(() => {
+      replacePointToForm();
+      document.addEventListener(`keydown`, onEscKeyDown);
+    });
+
+    eventEditComponent.setFormSubmitHandler(() => {
+      replaceFormToPoint();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    });
+
+    renderElement(this._eventsListElement, eventComponent, RENDER_POSITION.BEFOREEND);
   }
 
   _renderDay() {
     renderElement(this._daysComponent, this._dayComponent, RENDER_POSITION.BEFOREEND);
   }
 
-  _renderEvent(event, eventsListElement) {
-    renderElement(eventsListElement, renderEvent(event), RENDER_POSITION.BEFOREEND);
+  _renderDays() {
+    renderElement(this._boardContainer, this._daysComponent, RENDER_POSITION.BEFOREEND);
+
+    splitEventsByDays(this._boardEvent).forEach((eventsByDay, index) => {
+      this._dayDate = eventsByDay[0];
+      this._dayComponent = new DayView(this._dayDate, index + 1);
+      this._eventsListElement = this._dayComponent.getElement().querySelector(`#trip-events__list-${index + 1}`);
+
+      eventsByDay.slice(1).forEach((event) => {
+        this._renderEvent(event);
+      });
+
+      this._renderDay();
+    });
+  }
+
+  _renderSorting() {
+    renderElement(this._boardContainer, this._sortingComponent, RENDER_POSITION.BEFOREEND);
   }
 
   _renderNoEdit() {
@@ -49,22 +92,8 @@ export default class Board {
       this._renderNoEdit();
       return;
     }
-    
-    const eventsByDays = splitEventsByDays(this._boardEvent);
-    
+
     this._renderSorting();
     this._renderDays();
-    
-    eventsByDays.forEach((eventsByDay, index) => {
-      const dayDate = eventsByDay[0];
-      this._dayComponent = new DayView(dayDate, index + 1);
-      const eventsListElement = this._dayComponent.getElement().querySelector(`#trip-events__list-${index + 1}`);
-  
-      eventsByDay.slice(1).forEach((event) => {
-        this._renderEvent(event, eventsListElement);
-      });
-      
-      this._renderDay()
-    });
   }
 }
