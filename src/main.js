0fa0e9a8f7b0611cf0da5e9e2cd64ff3cbb0becb
Litvinov Valery <va-litv@yandex.ru>
2020-08-28 15:32:41 +0300

    M5T14 used presentor in main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import {EVENT_COUNT, RENDER_POSITION} from "./const";
-import {TripInfoView, NavigationControllerView, EventFiltrationView, TotalPriceView, SortingView, EventEditorView, DaysView, DayView, EventView, NoEventView} from "./view/index";
-import {generateEvent, DESTINATIONS} from "./mock/event";
-import {splitEventsByDays} from "./utils/event";
-import {renderElement, replaceElement} from "./utils/render";
+import {NavigationControllerView, EventFiltrationView} from "./view/index";
+import {generateEvent} from "./mock/event";
+import {renderElement} from "./utils/render";
+import BoardPresenter from "./presenter/board.js";
 
 const events = new Array(EVENT_COUNT).fill().map(generateEvent).sort((a, b) => a.dateStart - b.dateStart);
 
@@ -13,68 +13,9 @@ const eventsElement = siteMainElement.querySelector(`.trip-events`);
 const tripControlsFirstElement = tripElement.querySelector(`.trip-controls > h2:first-child`);
 const tripControlsSecondElement = tripElement.querySelector(`.trip-controls > h2:last-child`);
 
-const renderEvent = (event) => {
-  const eventComponent = new EventView(event);
-  const eventEditComponent = new EventEditorView(event, DESTINATIONS);
-
-  const replacePointToForm = () => {
-    replaceElement(eventEditComponent, eventComponent);
-  };
-
-  const replaceFormToPoint = () => {
-    replaceElement(eventComponent, eventEditComponent);
-  };
-
-  const onEscKeyDown = (evt) => {
-    if (evt.key === `Escape`) {
-      replaceFormToPoint();
-      document.removeEventListener(`keydown`, onEscKeyDown);
-    }
-  };
-
-  eventComponent.setEditClickHandler(() => {
-    replacePointToForm();
-    document.addEventListener(`keydown`, onEscKeyDown);
-  });
-
-  eventEditComponent.setFormSubmitHandler(() => {
-    replaceFormToPoint();
-    document.removeEventListener(`keydown`, onEscKeyDown);
-  });
-
-  return eventComponent.getElement();
-};
-
-const renderBoard = (container, boardEvents) => {
-  const tripInfoComponent = new TripInfoView(boardEvents);
-
-  renderElement(tripElement, tripInfoComponent, RENDER_POSITION.AFTERBEGIN);
-  renderElement(tripInfoComponent, new TotalPriceView(), RENDER_POSITION.BEFOREEND);
-
-  if (boardEvents.length === 0) {
-    renderElement(container, new NoEventView(), RENDER_POSITION.BEFOREEND);
-    return;
-  }
-
-  const daysComponent = new DaysView();
-  const eventsByDays = splitEventsByDays(boardEvents);
-
-  renderElement(container, new SortingView(), RENDER_POSITION.BEFOREEND);
-  renderElement(container, daysComponent, RENDER_POSITION.BEFOREEND);
-
-  eventsByDays.forEach((eventsByDay, index) => {
-    const dayDate = eventsByDay[0];
-    const dayComponent = new DayView(dayDate, index + 1);
-    const eventsListElement = dayComponent.getElement().querySelector(`#trip-events__list-${index + 1}`);
-
-    eventsByDay.slice(1).forEach((event) => {
-      renderElement(eventsListElement, renderEvent(event), RENDER_POSITION.BEFOREEND);
-    });
-
-    renderElement(daysComponent, dayComponent, RENDER_POSITION.BEFOREEND);
-  });
-};
+const boardPresenter = new BoardPresenter(eventsElement, tripElement);
 
 renderElement(tripControlsFirstElement, new NavigationControllerView(), RENDER_POSITION.AFTEREND);
 renderElement(tripControlsSecondElement, new EventFiltrationView(), RENDER_POSITION.AFTEREND);
-renderBoard(eventsElement, events);
+
+boardPresenter.init(events);
