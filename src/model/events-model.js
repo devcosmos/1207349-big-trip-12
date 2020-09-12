c8cf96432d0f6a8bd2cfd397ffb3d7db06f87ede
Litvinov Valery <va-litv@yandex.ru>
2020-09-12 21:24:47 +0300

    M7T22 added methods in eventsModel

--- src/model/events-model.js
+++ src/model/events-model.js
@@ -6,11 +6,51 @@ export default class EventsModel extends Observer {
     this._events = [];
   }
 
-  setevents(events) {
+  setEvents(events) {
     this._events = events.slice();
   }
 
-  getevents() {
+  getEvents() {
     return this._events;
+  }
+
+  updateEvent(updateType, update) {
+    const index = this._events.findIndex((event) => event.id === update.id);
+
+    if (index === -1) {
+      throw new Error(`Can't update unexisting event`);
+    }
+
+    this._events = [
+      ...this._events.slice(0, index),
+      update,
+      ...this._events.slice(index + 1)
+    ];
+
+    this._notify(updateType, update);
+  }
+
+  addEvent(updateType, update) {
+    this._events = [
+      update,
+      ...this._events
+    ];
+
+    this._notify(updateType, update);
+  }
+
+  deleteEvent(updateType, update) {
+    const index = this._events.findIndex((event) => event.id === update.id);
+
+    if (index === -1) {
+      throw new Error(`Can't delete unexisting event`);
+    }
+
+    this._events = [
+      ...this._events.slice(0, index),
+      ...this._events.slice(index + 1)
+    ];
+
+    this._notify(updateType);
   }
 }
