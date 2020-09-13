10e007555bfa554fa6f0b8380d7838da1f7d5d03
Litvinov Valery <va-litv@yandex.ru>
2020-09-13 11:42:58 +0300

    M7T22 created UserAction and UpdateType

--- src/const.js
+++ src/const.js
@@ -32,3 +32,14 @@ export const EventStatus = {
   DEFAULT: `DEFAULT`,
   EDITING: `EDITING`
 };
+
+export const UserAction = {
+  UPDATE_EVENT: `UPDATE_EVENT`,
+  ADD_EVENT: `ADD_EVENT`,
+  DELETE_EVENT: `DELETE_EVENT`
+};
+
+export const UpdateType = {
+  EVENT: `EVENT`,
+  TRIP: `TRIP`,
+};
