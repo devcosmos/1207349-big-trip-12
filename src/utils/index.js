7c1891eefc09dd2b03c089fcf9731fdea69c7deb
Litvinov Valery <va-litv@yandex.ru>
2020-09-22 15:55:00 +0300

    M9T29 created provider

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,6 @@
 export {getDateAtDefaultFormat, getDateAtShortFormat, getDateAtSystemFormat, getDurationTime, getTimeAtDefaultFormat, getDurationInHours} from "./date-formatters";
 export {splitEventsByDays, sortEventsByDuration, sortEventsByPrice, sortEventsByDate, splitEventsByChartType} from "./event";
 export {removeElement, renderElement, replaceElement, createElement} from "./render";
+export {isOnline} from "./common";
 export {filter} from "./filter";
 export {default as Observer} from "./observer";
