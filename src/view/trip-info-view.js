f300f3d3bcc4a2d744bd6cce989dc1f45584fd85
Litvinov Valery <va-litv@yandex.ru>
2020-09-24 13:03:03 +0300

    refactored trip info view

--- src/view/trip-info-view.js
+++ src/view/trip-info-view.js
@@ -6,33 +6,35 @@ const getTripRoute = (tripEvents) => {
   const uniqueCities = Array.from(new Set(cities));
 
   return (uniqueCities.length > 3)
-    ? cities[0] + `&nbsp;&mdash;&nbsp;...&nbsp;&mdash;&nbsp;` + cities[cities.length - 1]
+    ? cities[0] + ` &mdash; ... &mdash; ` + cities[cities.length - 1]
     : uniqueCities.join(` — `);
 };
 
 const getTripDateInterval = (tripEvents) => {
-  const start = getDateAtShortFormat(tripEvents[0].dateStart).split(` `);
-  const end = getDateAtShortFormat(tripEvents[tripEvents.length - 1].dateEnd).split(` `);
-  if (start.toString() === end.toString()) {
-    return `${start[1]}&nbsp;${start[0]}`;
+  const startDate = getDateAtShortFormat(tripEvents[0].dateStart);
+  const endDate = getDateAtShortFormat(tripEvents[tripEvents.length - 1].dateEnd);
+  const splitStartDate = startDate.split(` `);
+  const splitEndDate = endDate.split(` `);
+  if (startDate === endDate) {
+    return `${splitStartDate[1]} ${splitStartDate[0]}`;
   }
 
-  return `${start[1]}&nbsp;${start[0]}&nbsp;&mdash;&nbsp;${end[1]}${start[0] === end[0] ? `` : `&nbsp;` + end[0] }`;
+  return `${splitStartDate[1]} ${splitStartDate[0]} &mdash; ${splitEndDate[1]}${splitStartDate[0] === splitEndDate[0] ? `` : ` ` + splitEndDate[0]}`;
 };
 
 const createTripInfoTemplate = (tripEvents) => {
   return (
     `<section class="trip-main__trip-info  trip-info">
-      ${tripEvents === null || tripEvents.length === 0 ? `` : `<div class="trip-info__main">
+      ${tripEvents.length ? `<div class="trip-info__main"> 
         <h1 class="trip-info__title">${getTripRoute(tripEvents)}</h1>
         <p class="trip-info__dates">${getTripDateInterval(tripEvents)}</p>
-      </div>` }
+      </div>` : `` }
     </section>`
   );
 };
 
 export default class TripInfoView extends AbstractView {
-  constructor(tripEvents = null) {
+  constructor(tripEvents) {
     super();
     this._tripEvents = tripEvents;
   }
