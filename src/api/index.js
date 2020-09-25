2dc35f8e461cc5f89ba1eea7bcdc56a1a58d89ad
Litvinov Valery <va-litv@yandex.ru>
2020-09-25 14:27:23 +0300

    fixed mistakes

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,3 @@
 export {default as Api} from "./api";
 export {default as Store} from "./store";
-export {default as ApiProxyCache} from "./api-proxy";
+export {default as ApiProxyCache} from "./api-proxy-cache";
